110c3bce496fe969ce21d25ad54f11d9433c2dc0
Solomon Hawk <solomon.hawk@viget.com>
2022-09-09 14:08:30 -0400

    Add basic configuration

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const list = [];
+
+console.log(list);
