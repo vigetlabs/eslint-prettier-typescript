a927a2ce9cd0acdcec9326ab11bf1fb2b4b9aabf
Andrew Thomas <andrewmthomas87@gmail.com>
2023-02-07 10:48:50 -0700

    Add prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  printWidth: 80,
+  proseWrap: 'preserve',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
